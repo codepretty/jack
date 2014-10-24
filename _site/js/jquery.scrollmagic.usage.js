ca3c9df536dfb5a3eb778c13967a9f0476b229ec
Brigette <brigetl@microsoft.com>
2014-10-24 10:08:38 -0700

    started a control for showing Jack's favorite things

--- _site/js/jquery.scrollmagic.usage.js
+++ _site/js/jquery.scrollmagic.usage.js
@@ -1,9 +1,9 @@
 $(function() {
-	// init controller
-	var controller = new ScrollMagic();
+  // init controller
+  var controller = new ScrollMagic();
 
-	// assign handler "scene" and add it to Controller
-	var scene = new ScrollScene({duration: 100})
-	                .setPin("#pin-me")   // pins the element for a scroll distance of 100px
-	                .addTo(controller);
+  // assign handler "scene" and add it to Controller
+  var scene = new ScrollScene({duration: 100})
+          .setPin("#pin-me")   // pins the element for a scroll distance of 100px
+          .addTo(controller);
 });
